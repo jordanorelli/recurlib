6bb53a259b84477c91a7d42e1a5af1bf9bff621d
Jordan Orelli <jordanorelli@gmail.com>
2011-10-23 21:16:33 -0400

    added dev status

--- setup.py
+++ setup.py
@@ -1,19 +1,20 @@
 import os
 from setuptools import setup
 
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
-
 setup(
-    name = "recurlib",
-    version = "0.0.7",
-    author = "Jordan Orelli",
-    author_email = "jordanorelli@gmail.com",
-    description = ("A client for interacting with Recurly, a subscription \
+    name="recurlib",
+    version="0.0.7",
+    author="Jordan Orelli",
+    author_email="jordanorelli@gmail.com",
+    description=("A client for interacting with Recurly, a subscription \
                    billing service."),
-    license = "MIT",
-    keywords = "subscriptions billing payments ecommerce",
-    url = "https://github.com/jordanorelli/recurlib",
-    packages = ['recurly', 'recurly.managers', 'recurly.models', 'tests'],
-    install_requires = ['python-dateutil < 2.0', 'requests'],
+    license="MIT",
+    keywords="subscriptions billing payments ecommerce",
+    url="https://github.com/jordanorelli/recurlib",
+    packages=['recurly', 'recurly.managers', 'recurly.models', 'tests'],
+    install_requires=['python-dateutil < 2.0', 'requests'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+    ],
 )
